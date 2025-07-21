f08a852ab4b5acbf86e4dc68469278a4d6c15304
Ashutoshpal63 <ashutoshpal646@gmail.com>
2025-07-21 12:45:28 +0530

    Update main.jsx

--- local-shop-frontend/src/main.jsx
+++ local-shop-frontend/src/main.jsx
@@ -1,8 +1,31 @@
+// src/main.jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+
+// --- START: Leaflet Icon Fix ---
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    iconRetinaUrl: iconRetina,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+// --- END: Leaflet Icon Fix ---
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
